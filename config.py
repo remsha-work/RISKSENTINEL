e980f19b3a7157d33f4c7e82e4e4ae2608beaf8b
remsha-work <remsha.work@gmail.com>
2026-02-06 11:51:15 +0530

    Add encrypted SMTP config

---
+++ config.py
@@ -0,0 +1,4 @@
+ENCRYPTED_SMTP_PASS = "gAAAAABphYX1ZSI068YwYmSmtha2WgaoYve3CVTMl64AfzNLELq2sF25R_8YM5igA9D95XXBVaKUmb36mpBwYBsv3izKMftOp63xQ2AoLkgxIBSJclZQuxY="
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+SMTP_USER = "risksentinel.help@gmail.com"  # Change this to YOUR email
